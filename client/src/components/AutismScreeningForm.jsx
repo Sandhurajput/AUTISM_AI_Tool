a95d7ee6f48fd756b9037fbfecb5dd8ef026bc83
AI_Chatboot <sandhyakuamri24@navgurukul.org>
2025-10-19 20:50:41 +0530

    added link only

--- client/src/components/AutismScreeningForm.jsx
+++ client/src/components/AutismScreeningForm.jsx
@@ -35,7 +35,7 @@ const AutismScreeningForm = () => {
     };
 
    try {
-      const response = await fetch("http://localhost:5000/api/analyze", {
+      const response = await fetch(import.meta.env.VITE_API_URL, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
